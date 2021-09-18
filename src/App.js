65fb08904d2fefa26818c10b40daaf06531039ec
unknown <contactdamilolaayodele@gmail.com>
2021-09-18 19:31:34 +0100

    Adding files to calculator app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,72 @@
 import { useState } from "react";
-const CreateDigits = () => {
 
-  const updateCalc = (value) => {
-    setCalc(calc + value);
-  }
-  const [calc, setCalc] = useState("");
+const CreateDigits = ({updateCalc}) => {
+
 
   return [...Array(9)].map((i, idx) => (
-	<button onClick= {() => updateCalc(i,idx)}
-  key={`btn ${idx}`}>{idx + 1}</button>
+    <button onClick={() => updateCalc(`${idx + 1}`)} key={`btn ${idx}`}>
+      {idx + 1}
+    </button>
   ));
 };
 
 function App() {
   const [calc, setCalc] = useState("");
-  const [result, setResult] = useState ("");
-  const operators = ['/', '*', '+', '-', '.'];
+  const [result, setResult] = useState("");
+  const ops = ["/", "*", "+", "-", "."];
 
   const updateCalc = value => {
+    if (
+      ops.includes(value) && calc === '' || 
+      ops.includes(value) && ops.includes(calc.slice(-1)
+        )
+
+    )     { 
+      return;
+      
+    }
+
     setCalc(calc + value);
+    if (! ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+  
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+    const value= calc.slice(0, -1);
+
+    setCalc(value);
   }
 
-  
   return (
     <div className="App">
       <div className="calculator">
         <div className="display">
-         { result ? <span> (0) </span> : '' }
-          {calc || "0"} 
+          {result ? <span> ({result}) </span> : ''}
+          {calc || "0"}
         </div>
 
         <div className="operators">
-          <button onClick= {() => updateCalc('/') }>/</button>
-          <button onClick= {() => updateCalc('*')}>*</button>
-          <button onClick= {() => updateCalc('+')}>+</button>
-          <button onClick= {() => updateCalc('-')}>-</button>
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
 
-          <button>DEL</button>
+          <button onClick={deleteLast} >DEL</button>
         </div>
 
         <div className="digits">
-          <CreateDigits  />
-          <button onClick= {() => updateCalc('0')}>0</button>
-          <button onClick= {() => updateCalc('.')}>.</button> 
-         
-          <button> =</button>
+          <CreateDigits updateCalc={updateCalc} />
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+
+          <button onClick={calculate}> =</button>
         </div>
       </div>
     </div>
